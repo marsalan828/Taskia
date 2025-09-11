10b9cd728c761591cc76d00407373fd5f7c87fd2
marsalan828 <marsalan828@gmail.com>
2025-09-11 15:48:47 +0500

    Implemented Login and Register user pages and functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
-import "./App.css";
-import Board from "./components/Board";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import "./App.css";
+import Board from "./pages/Board";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
 
 function App() {
-  return <Board />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/board" element={<Board />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
